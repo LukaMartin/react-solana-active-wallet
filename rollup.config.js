e94b92b0f93237026f1e891247e126d1c44e3657
Luka Martin <ldm3334@gmail.com>
2024-09-12 00:56:17 +1000

    First commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,38 @@
-const packageJson = require('./package.json');
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
-import json from '@rollup/plugin-json';
+const packageJson = require("./package.json");
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            }
-        ],
-        plugins: [
-            peerDepsExternal(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            resolve(),
-            terser(),
-            json(),
-        ],
-        external: ['react', 'react-dom'],
-    },
-    {
-        input: 'src/index.ts',
-        output: [{ file: packageJson.types }],
-        plugins: [dts.default()],
-    }
-]
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      resolve(),
+      terser(),
+      json(),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: packageJson.types }],
+    plugins: [dts.default()],
+  },
+];
